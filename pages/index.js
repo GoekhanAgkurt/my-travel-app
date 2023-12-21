50d72d64a4b7ec0b36b5714e6715e0e10c0142fa
Goekhan Agkurt <g.agkurt@hotmail.de>
2023-12-21 09:53:25 +0100

    add edit travel function

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,15 @@
 import TravelList from "../components/travelList/travelList";
 
-export default function HomePage({ travels, onDeleteTravel }) {
+export default function HomePage({ travels, onDeleteTravel, onEditTravel }) {
   return (
     <div>
       <h1>Travel App</h1>
 
-      <TravelList travels={travels} onDeleteTravel={onDeleteTravel} />
+      <TravelList
+        travels={travels}
+        onDeleteTravel={onDeleteTravel}
+        onEditTravel={onEditTravel}
+      />
     </div>
   );
 }
--- pages/travels/[id].js
+++ pages/travels/[id].js
@@ -1,11 +1,14 @@
 import Link from "next/link";
 
 import { useRouter } from "next/router";
+import { useState } from "react";
 
-export default function Detailspage({ travels, onDeleteTravel }) {
+export default function Detailspage({ travels, onDeleteTravel, onEditTravel }) {
   const router = useRouter(); //  (next.js) greift auf die Informationen der aktuellen Route
   const { isReady } = router; // (next.js) wartet bis die router-initialisierung abgeschlossen ist
   const { id } = router.query; // speichert die id Nummer von z.b. /meine-seite?id=123 aus der aktuellen URL in id
+
+  const [isEdit, setIsEdit] = useState(false);
 
   const travel = travels.find((travel) => travel.id === id);
   if (!isReady) return "please wait";
@@ -16,14 +19,67 @@ export default function Detailspage({ travels, onDeleteTravel }) {
     router.push("/");
   }
 
+  function editTravel(event) {
+    event.preventDefault();
+
+    const formData = new FormData(event.target);
+    const data = Object.fromEntries(formData);
+
+    const editedTravel = {
+      id: id,
+      title: data.title,
+      description: data.description,
+      location: data.location,
+    };
+
+    onEditTravel(editedTravel);
+    setIsEdit(false);
+  }
+
   return (
     <>
-      <h3>Details Page</h3>
-      <h3>{travel.title}</h3>
-      <p>{travel.description}</p>
-      <p>{travel.location}</p>
-      <Link href="/">Back to list</Link>
-      <button onClick={deleteTravel}>Delete</button>
+      {!isEdit ? (
+        <>
+          <h3>Details Page</h3>
+          <p>{travel.title}</p>
+          <p>{travel.description}</p>
+          <p>{travel.local}</p>
+
+          <Link href="/"> Back to list</Link>
+          <button onClick={() => setIsEdit(true)}>Edit</button>
+          <button onClick={deleteTravel}>Delete</button>
+        </>
+      ) : (
+        <form onSubmit={editTravel}>
+          <label>Title</label>
+          <input
+            type="text"
+            placeholder="z.B. Karibik Urlaub"
+            id="title"
+            name="title"
+            defaultValue={travel.title}
+          />
+
+          <label>Description</label>
+          <input
+            type="text"
+            placeholder="z.B. Schwimmen im Meer"
+            id="description"
+            name="description"
+            defaultValue={travel.description}
+          />
+
+          <label>Location</label>
+          <input
+            type="text"
+            placeholder="z.B. Nordsee, Deutschland"
+            id="location"
+            name="location"
+            defaultValue={travel.location}
+          />
+          <button type="submit">Save changes</button>
+        </form>
+      )}
     </>
   );
 }
